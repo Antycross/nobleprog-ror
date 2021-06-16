8457c7857a4f55a85c5d1c8b5efe11f6797c5ed5
Maciej Wozniak <maciek12246@o2.pl>
2021-06-16 21:18:52 +0000

    day_one_final

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,12 +2,16 @@ class StaticPagesController < ApplicationController
     def privacy
         view_to_render = if params[:lang] == 'pl'
             "privacy_pl"
-        elsif params[:lang] == 'eng'
-            "privacy_eng"
-        else
-            "privacy"
-        end
-        render view_to_render
+            elsif params[:lang] == 'es'
+                "privacy_es"
+            else 
+                @privacy_text = Faker::Lorem.paragraphs(number: 6)
+                "privacy"
+            end
+
+        render view_to_render#, layout: 'blogpost'
     end
-    
+
+    def current_time
+    end
 end
