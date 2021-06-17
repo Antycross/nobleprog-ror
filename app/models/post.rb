a9da806f934683137078a0ebb772a1bd9b2f9680
Maciej Wozniak <maciek12246@o2.pl>
2021-06-17 09:40:48 +0000

    web posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,3 @@
 class Post < ApplicationRecord
-
-    def content_snapshot
-        content.split('.').first.strip
-    end
-
-    def content_keywords(count)
-        content.split('.').
-        map {|x|x.split(' ')}.
-        flatten.
-        sample(count)
-    end
+  belongs_to :user
 end
